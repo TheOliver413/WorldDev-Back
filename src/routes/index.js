d3ccd5f278588520115018d17b832e1983d41d39
Angeles Gallardo <angelesgallardo.mlg@gmail.com>
2022-10-12 19:17:14 -0300

    index actualizado

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,9 @@
+require('dotenv').config();
 const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-
-
 const router = Router();
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
 
+router.use('/filtersH', filtersHRoutes)
+router.use('/filtersR', filtersRRoutes)
 
 module.exports = router;
