9aba906628598c2ec686954a577829c7af39858e
Franco <francollopard@gmail.com>
2022-10-13 19:14:22 -0300

    modificacion rutas

--- src/middlewares/roomRoutes.js
+++ src/middlewares/roomRoutes.js
@@ -1,21 +1,27 @@
 const {Router} = require('express')
 const {Room} = require('../db')
-const {getAllRooms, getRoomsByName, getRoomById ,createRoom, updateRoom, deleteRoom} = require('../controllers/room')
+const { getRoomsByName,getRoomById ,createRoom, updateRoom, deleteRoom} = require('../controllers/room')
 
 const router = Router()
 
+
 router.get('', async (req, res, next)=>{
-    const {name} = req.query
-    try {
-        if(name){
-           return res.json(await getRoomsByName(name)) 
-        }else{
-            return res.json(await getAllRooms())
+        const {name} = req.query
+        try {
+              return res.json(await getRoomsByName(name)) 
+        } catch (error) {
+            next(error)
         }
-    } catch (error) {
-        next(error)
-    }
-})
+    })
+
+// router.get('', async (req, res, next)=>{
+//     const {id} = req.query
+//     try {
+//           return res.json(await getAllRoomsOfHotel(id)) 
+//     } catch (error) {
+//         next(error)
+//     }
+// })
 
 router.get('/:id', async (req, res, next)=>{
     const {id} = req.params
