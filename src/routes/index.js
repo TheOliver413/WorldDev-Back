2455f503e82d7d41ad1d5865efb4a436a020910f
Angeles Gallardo <angelesgallardo.mlg@gmail.com>
2022-10-13 19:24:28 -0300

    cambios index

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,12 @@ const { Router } = require('express');
 const router = Router();
 const roomRoutes = require('../middlewares/roomRoutes')
 const hotelRoutes = require('../middlewares/hotelRoutes')
+const filtersHRoutes = require('../middlewares/filtersHRoutes')
+const filtersRRoutes = require('../middlewares/filtersRRoutes')
 
- router.use('/filtersHotels', filtersHRoutes)
- router.use('/filtersRooms', filtersRRoutes)
-
-router.use('/rooms',roomRoutes)
-router.use('/hotels', hotelRoutes)
+// router.use('/rooms',roomRoutes)
+// router.use('/hotels', hotelRoutes)
+// router.use('/filtersHotels', filtersHRoutes)
+// router.use('/filtersRooms', filtersRRoutes)
 
 module.exports = router;
