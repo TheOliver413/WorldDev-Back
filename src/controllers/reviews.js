fc43f190cf0d38d36554eb465e0084455cc29a2d
solch4 <sol.maldonado.0209@gmail.com>
2022-10-31 13:43:16 -0300

    review user validation

--- src/controllers/reviews.js
+++ src/controllers/reviews.js
@@ -1,63 +1,69 @@
-const { Hotel, Location, ServicesHotel, ServicesRoom, Room, Event,Hotel_Event, Review} = require('../db')
+const { Hotel, Location, Review } = require("../db");
 
-const { Op } = require('sequelize')
+async function getHotelReviewsById(id) {
+  try {
+    console.log(id);
+    let hotelReviewsFinded = await Hotel.findByPk(id, {
+      include: [
+        {
+          model: Review,
+          attributes: ["id", "name", "rating", "comment", "user"],
+          through: {
+            attributes: [],
+          },
+        },
+        {
+          model: Location,
+          attributes: ["city", "state", "department"],
+          through: {
+            attributes: [],
+          },
+        },
+      ],
+    });
 
-async function getHotelReviewsById(id){
-    try {
-        console.log(id)
-        let hotelReviewsFinded = await Hotel.findByPk(id,{ include: [{
-            model: Review,
-            attributes: ['id', "name", "rating", "comment", "user"],
-            through: {
-                attributes: []
-            }
-        },{
-            model: Location,
-            attributes: ['city', 'state','department'],
-            through: {
-                attributes: []
-            }
-        }]})
-
-        if(hotelReviewsFinded){
-            return {hotel: hotelReviewsFinded.name ,reviews: hotelReviewsFinded.Reviews}
-        }
-            return 'No reviews found'
-    } catch (error) {
-        console.log(error)
+    if (hotelReviewsFinded) {
+      return {
+        hotel: hotelReviewsFinded.name,
+        reviews: hotelReviewsFinded.Reviews,
+      };
     }
+    return "No reviews found";
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-async function createReview({ idHotel, name, rating, comment, user}){
-try {
-    let hotelFinded = await Hotel.findByPk(idHotel)
+async function createReview({ idHotel, name, rating, comment, user }) {
+  const hotelFinded = await Hotel.findByPk(idHotel);
+  const reviews = await Review.findAll();
+  const allReviewsUserId = reviews.map((r) => r.dataValues.user);
 
-    let reviewCreated = await Review.create({
-        name:name, 
-        rating:rating, 
-        comment:comment, 
-        user:user
-    })
+  if (allReviewsUserId.includes(user)) throw `User ${name} already reviewed this hotel.`
 
-    hotelFinded.addReviews(reviewCreated)
+  const reviewCreated = await Review.create({
+    name,
+    rating,
+    comment,
+    user,
+  });
 
-    return 'Review created'
-} catch (error) {
-    console.log(error)
+  hotelFinded.addReviews(reviewCreated);
+
+  return "Review submitted successfully";
 }
-}
 
-async function deleteReview(id){
-    await Review.destroy({
-        where:{
-            id: id
-        }
-    })
-    return 'Deleted'
+async function deleteReview(id) {
+  await Review.destroy({
+    where: {
+      id: id,
+    },
+  });
+  return "Deleted";
 }
 
 module.exports = {
-    createReview,
-    deleteReview,
-    getHotelReviewsById
-}
+  createReview,
+  deleteReview,
+  getHotelReviewsById,
+};
