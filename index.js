a5e24af271d544001e9022ec1e43a69658b6dab6
TheOliver413 <oliverchemas@hotmail.com>
2022-11-16 09:31:11 -0500

    Deploy

--- index.js
+++ index.js
@@ -21,10 +21,9 @@ require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
+  server.listen(process.env.PORT || 3001 , () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
